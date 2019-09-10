7733a0c13f3c864d04799fea8ad3b5956642e01c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-10 08:31:31 +0000

    Deploy qooxdoo/qxl.dialog to github.com/qooxdoo/qxl.dialog.git:gh-pages

---
+++ qxl.dialog.demo/resources.js
@@ -0,0 +1,718 @@
+qx.$$packageData['0'] = {
+  "locales": {
+    "C": {
+      "alternateQuotationEnd": "’",
+      "alternateQuotationStart": "‘",
+      "quotationEnd": "”",
+      "quotationStart": "“",
+      "cldr_am": "AM",
+      "cldr_pm": "PM",
+      "cldr_date_format_full": "EEEE, MMMM d, y",
+      "cldr_date_format_long": "MMMM d, y",
+      "cldr_date_format_medium": "MMM d, y",
+      "cldr_date_format_short": "M/d/yy",
+      "cldr_date_time_format_Ed": "d E",
+      "cldr_date_time_format_Hm": "HH:mm",
+      "cldr_date_time_format_Hms": "HH:mm:ss",
+      "cldr_date_time_format_M": "L",
+      "cldr_date_time_format_MEd": "E, M/d",
+      "cldr_date_time_format_MMM": "LLL",
+      "cldr_date_time_format_MMMEd": "E, MMM d",
+      "cldr_date_time_format_MMMd": "MMM d",
+      "cldr_date_time_format_Md": "M/d",
+      "cldr_date_time_format_d": "d",
+      "cldr_date_time_format_hm": "h:mm a",
+      "cldr_date_time_format_hms": "h:mm:ss a",
+      "cldr_date_time_format_ms": "mm:ss",
+      "cldr_date_time_format_y": "y",
+      "cldr_date_time_format_yM": "M/y",
+      "cldr_date_time_format_yMEd": "E, M/d/y",
+      "cldr_date_time_format_yMMM": "MMM y",
+      "cldr_date_time_format_yMMMEd": "E, MMM d, y",
+      "cldr_date_time_format_yMMMd": "MMM d, y",
+      "cldr_date_time_format_yMd": "M/d/y",
+      "cldr_date_time_format_yQ": null,
+      "cldr_date_time_format_yQQQ": "QQQ y",
+      "cldr_day_format_abbreviated_fri": "Fri",
+      "cldr_day_format_abbreviated_mon": "Mon",
+      "cldr_day_format_abbreviated_sat": "Sat",
+      "cldr_day_format_abbreviated_sun": "Sun",
+      "cldr_day_format_abbreviated_thu": "Thu",
+      "cldr_day_format_abbreviated_tue": "Tue",
+      "cldr_day_format_abbreviated_wed": "Wed",
+      "cldr_day_format_wide_fri": "Friday",
+      "cldr_day_format_wide_mon": "Monday",
+      "cldr_day_format_wide_sat": "Saturday",
+      "cldr_day_format_wide_sun": "Sunday",
+      "cldr_day_format_wide_thu": "Thursday",
+      "cldr_day_format_wide_tue": "Tuesday",
+      "cldr_day_format_wide_wed": "Wednesday",
+      "cldr_day_stand-alone_narrow_fri": "F",
+      "cldr_day_stand-alone_narrow_mon": "M",
+      "cldr_day_stand-alone_narrow_sat": "S",
+      "cldr_day_stand-alone_narrow_sun": "S",
+      "cldr_day_stand-alone_narrow_thu": "T",
+      "cldr_day_stand-alone_narrow_tue": "T",
+      "cldr_day_stand-alone_narrow_wed": "W",
+      "cldr_month_format_abbreviated_1": "Jan",
+      "cldr_month_format_abbreviated_2": "Feb",
+      "cldr_month_format_abbreviated_3": "Mar",
+      "cldr_month_format_abbreviated_4": "Apr",
+      "cldr_month_format_abbreviated_5": "May",
+      "cldr_month_format_abbreviated_6": "Jun",
+      "cldr_month_format_abbreviated_7": "Jul",
+      "cldr_month_format_abbreviated_8": "Aug",
+      "cldr_month_format_abbreviated_9": "Sep",
+      "cldr_month_format_abbreviated_10": "Oct",
+      "cldr_month_format_abbreviated_11": "Nov",
+      "cldr_month_format_abbreviated_12": "Dec",
+      "cldr_month_format_wide_1": "January",
+      "cldr_month_format_wide_2": "February",
+      "cldr_month_format_wide_3": "March",
+      "cldr_month_format_wide_4": "April",
+      "cldr_month_format_wide_5": "May",
+      "cldr_month_format_wide_6": "June",
+      "cldr_month_format_wide_7": "July",
+      "cldr_month_format_wide_8": "August",
+      "cldr_month_format_wide_9": "September",
+      "cldr_month_format_wide_10": "October",
+      "cldr_month_format_wide_11": "November",
+      "cldr_month_format_wide_12": "December",
+      "cldr_month_stand-alone_narrow_1": "J",
+      "cldr_month_stand-alone_narrow_2": "F",
+      "cldr_month_stand-alone_narrow_3": "M",
+      "cldr_month_stand-alone_narrow_4": "A",
+      "cldr_month_stand-alone_narrow_5": "M",
+      "cldr_month_stand-alone_narrow_6": "J",
+      "cldr_month_stand-alone_narrow_7": "J",
+      "cldr_month_stand-alone_narrow_8": "A",
+      "cldr_month_stand-alone_narrow_9": "S",
+      "cldr_month_stand-alone_narrow_10": "O",
+      "cldr_month_stand-alone_narrow_11": "N",
+      "cldr_month_stand-alone_narrow_12": "D",
+      "cldr_number_decimal_separator": ".",
+      "cldr_number_group_separator": ",",
+      "cldr_number_percent_format": "#,##0%",
+      "day": [
+        "day"
+      ],
+      "dayperiod": [
+        "AM/PM",
+        "am/pm"
+      ],
+      "era": [
+        "era"
+      ],
+      "hour": [
+        "hour"
+      ],
+      "minute": [
+        "minute"
+      ],
+      "month": [
+        "month"
+      ],
+      "second": [
+        "second"
+      ],
+      "week": [
+        "week"
+      ],
+      "weekday": [
+        "day of the week"
+      ],
+      "year": [
+        "year"
+      ],
+      "zone": [
+        "time zone"
+      ]
+    },
+    "en": {
+      "alternateQuotationEnd": "’",
+      "alternateQuotationStart": "‘",
+      "quotationEnd": "”",
+      "quotationStart": "“",
+      "cldr_am": "AM",
+      "cldr_pm": "PM",
+      "cldr_date_format_full": "EEEE, MMMM d, y",
+      "cldr_date_format_long": "MMMM d, y",
+      "cldr_date_format_medium": "MMM d, y",
+      "cldr_date_format_short": "M/d/yy",
+      "cldr_date_time_format_Ed": "d E",
+      "cldr_date_time_format_Hm": "HH:mm",
+      "cldr_date_time_format_Hms": "HH:mm:ss",
+      "cldr_date_time_format_M": "L",
+      "cldr_date_time_format_MEd": "E, M/d",
+      "cldr_date_time_format_MMM": "LLL",
+      "cldr_date_time_format_MMMEd": "E, MMM d",
+      "cldr_date_time_format_MMMd": "MMM d",
+      "cldr_date_time_format_Md": "M/d",
+      "cldr_date_time_format_d": "d",
+      "cldr_date_time_format_hm": "h:mm a",
+      "cldr_date_time_format_hms": "h:mm:ss a",
+      "cldr_date_time_format_ms": "mm:ss",
+      "cldr_date_time_format_y": "y",
+      "cldr_date_time_format_yM": "M/y",
+      "cldr_date_time_format_yMEd": "E, M/d/y",
+      "cldr_date_time_format_yMMM": "MMM y",
+      "cldr_date_time_format_yMMMEd": "E, MMM d, y",
+      "cldr_date_time_format_yMMMd": "MMM d, y",
+      "cldr_date_time_format_yMd": "M/d/y",
+      "cldr_date_time_format_yQ": null,
+      "cldr_date_time_format_yQQQ": "QQQ y",
+      "cldr_day_format_abbreviated_fri": "Fri",
+      "cldr_day_format_abbreviated_mon": "Mon",
+      "cldr_day_format_abbreviated_sat": "Sat",
+      "cldr_day_format_abbreviated_sun": "Sun",
+      "cldr_day_format_abbreviated_thu": "Thu",
+      "cldr_day_format_abbreviated_tue": "Tue",
+      "cldr_day_format_abbreviated_wed": "Wed",
+      "cldr_day_format_wide_fri": "Friday",
+      "cldr_day_format_wide_mon": "Monday",
+      "cldr_day_format_wide_sat": "Saturday",
+      "cldr_day_format_wide_sun": "Sunday",
+      "cldr_day_format_wide_thu": "Thursday",
+      "cldr_day_format_wide_tue": "Tuesday",
+      "cldr_day_format_wide_wed": "Wednesday",
+      "cldr_day_stand-alone_narrow_fri": "F",
+      "cldr_day_stand-alone_narrow_mon": "M",
+      "cldr_day_stand-alone_narrow_sat": "S",
+      "cldr_day_stand-alone_narrow_sun": "S",
+      "cldr_day_stand-alone_narrow_thu": "T",
+      "cldr_day_stand-alone_narrow_tue": "T",
+      "cldr_day_stand-alone_narrow_wed": "W",
+      "cldr_month_format_abbreviated_1": "Jan",
+      "cldr_month_format_abbreviated_2": "Feb",
+      "cldr_month_format_abbreviated_3": "Mar",
+      "cldr_month_format_abbreviated_4": "Apr",
+      "cldr_month_format_abbreviated_5": "May",
+      "cldr_month_format_abbreviated_6": "Jun",
+      "cldr_month_format_abbreviated_7": "Jul",
+      "cldr_month_format_abbreviated_8": "Aug",
+      "cldr_month_format_abbreviated_9": "Sep",
+      "cldr_month_format_abbreviated_10": "Oct",
+      "cldr_month_format_abbreviated_11": "Nov",
+      "cldr_month_format_abbreviated_12": "Dec",
+      "cldr_month_format_wide_1": "January",
+      "cldr_month_format_wide_2": "February",
+      "cldr_month_format_wide_3": "March",
+      "cldr_month_format_wide_4": "April",
+      "cldr_month_format_wide_5": "May",
+      "cldr_month_format_wide_6": "June",
+      "cldr_month_format_wide_7": "July",
+      "cldr_month_format_wide_8": "August",
+      "cldr_month_format_wide_9": "September",
+      "cldr_month_format_wide_10": "October",
+      "cldr_month_format_wide_11": "November",
+      "cldr_month_format_wide_12": "December",
+      "cldr_month_stand-alone_narrow_1": "J",
+      "cldr_month_stand-alone_narrow_2": "F",
+      "cldr_month_stand-alone_narrow_3": "M",
+      "cldr_month_stand-alone_narrow_4": "A",
+      "cldr_month_stand-alone_narrow_5": "M",
+      "cldr_month_stand-alone_narrow_6": "J",
+      "cldr_month_stand-alone_narrow_7": "J",
+      "cldr_month_stand-alone_narrow_8": "A",
+      "cldr_month_stand-alone_narrow_9": "S",
+      "cldr_month_stand-alone_narrow_10": "O",
+      "cldr_month_stand-alone_narrow_11": "N",
+      "cldr_month_stand-alone_narrow_12": "D",
+      "cldr_number_decimal_separator": ".",
+      "cldr_number_group_separator": ",",
+      "cldr_number_percent_format": "#,##0%",
+      "day": [
+        "day"
+      ],
+      "dayperiod": [
+        "AM/PM",
+        "am/pm"
+      ],
+      "era": [
+        "era"
+      ],
+      "hour": [
+        "hour"
+      ],
+      "minute": [
+        "minute"
+      ],
+      "month": [
+        "month"
+      ],
+      "second": [
+        "second"
+      ],
+      "week": [
+        "week"
+      ],
+      "weekday": [
+        "day of the week"
+      ],
+      "year": [
+        "year"
+      ],
+      "zone": [
+        "time zone"
+      ]
+    }
+  },
+  "resources": {
+    "qx/decoration/Modern/arrows/down-invert.png": [
+      8,
+      5,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/down-small.png": [
+      5,
+      3,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/down.png": [
+      8,
+      5,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/forward.png": [
+      10,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/left.png": [
+      5,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/rewind.png": [
+      10,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/right-invert.png": [
+      5,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/right.png": [
+      5,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/up-invert.png": [
+      8,
+      5,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/up-small.png": [
+      5,
+      3,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/arrows/up.png": [
+      8,
+      5,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/colorselector/brightness-field.png": [
+      19,
+      256,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/colorselector/brightness-handle.gif": [
+      35,
+      11,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/colorselector/huesaturation-field.jpg": [
+      256,
+      256,
+      "jpg",
+      "qx"
+    ],
+    "qx/decoration/Modern/colorselector/huesaturation-handle.gif": [
+      11,
+      11,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/cursors/alias.gif": [
+      19,
+      15,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/cursors/copy.gif": [
+      19,
+      15,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/cursors/move.gif": [
+      13,
+      9,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/cursors/nodrop.gif": [
+      20,
+      20,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/form/checked.png": [
+      6,
+      6,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/form/tooltip-error-arrow-right.png": [
+      11,
+      14,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/form/tooltip-error-arrow.png": [
+      11,
+      14,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/form/undetermined.png": [
+      6,
+      2,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/menu/checkbox-invert.gif": [
+      16,
+      7,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/menu/checkbox.gif": [
+      16,
+      7,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/menu/radiobutton-invert.gif": [
+      16,
+      5,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/menu/radiobutton.gif": [
+      16,
+      5,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/scrollbar/scrollbar-down.png": [
+      6,
+      4,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/scrollbar/scrollbar-left.png": [
+      4,
+      6,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/scrollbar/scrollbar-right.png": [
+      4,
+      6,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/scrollbar/scrollbar-up.png": [
+      6,
+      4,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/splitpane/knob-horizontal.png": [
+      1,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/splitpane/knob-vertical.png": [
+      8,
+      1,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/table/ascending.png": [
+      8,
+      5,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/table/boolean-false.png": [
+      14,
+      14,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/table/boolean-true.png": [
+      14,
+      14,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/table/descending.png": [
+      8,
+      5,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/table/select-column-order.png": [
+      10,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/toolbar/toolbar-handle-knob.gif": [
+      1,
+      8,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/toolbar/toolbar-part.gif": [
+      7,
+      1,
+      "gif",
+      "qx"
+    ],
+    "qx/decoration/Modern/tree/closed-selected.png": [
+      8,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/tree/closed.png": [
+      8,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/tree/open-selected.png": [
+      8,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/tree/open.png": [
+      8,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/close-active-hovered.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/close-active.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/close-inactive.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/maximize-active-hovered.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/maximize-active.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/maximize-inactive.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/minimize-active-hovered.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/minimize-active.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/minimize-inactive.png": [
+      9,
+      9,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/restore-active-hovered.png": [
+      9,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/restore-active.png": [
+      9,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/decoration/Modern/window/restore-inactive.png": [
+      9,
+      8,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Oxygen/16/apps/office-calendar.png": [
+      16,
+      16,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Oxygen/22/actions/dialog-cancel.png": [
+      22,
+      22,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Oxygen/22/actions/dialog-ok.png": [
+      22,
+      22,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Oxygen/48/status/dialog-error.png": [
+      48,
+      48,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Oxygen/48/status/dialog-information.png": [
+      48,
+      48,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Oxygen/48/status/dialog-warning.png": [
+      48,
+      48,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Tango/16/apps/office-calendar.png": [
+      16,
+      16,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Tango/22/actions/dialog-cancel.png": [
+      22,
+      22,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Tango/22/actions/dialog-ok.png": [
+      22,
+      22,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Tango/48/status/dialog-error.png": [
+      48,
+      48,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Tango/48/status/dialog-information.png": [
+      48,
+      48,
+      "png",
+      "qx"
+    ],
+    "qx/icon/Tango/48/status/dialog-warning.png": [
+      48,
+      48,
+      "png",
+      "qx"
+    ],
+    "qx/static/blank.gif": [
+      1,
+      1,
+      "gif",
+      "qx"
+    ],
+    "qx/static/blank.png": [
+      1,
+      1,
+      "png",
+      "qx"
+    ],
+    "qxl/dialog/icon/IcoMoonFree/264-warning.svg": [
+      null,
+      null,
+      "svg",
+      "qxl.dialog"
+    ],
+    "qxl/dialog/icon/IcoMoonFree/269-info.svg": [
+      null,
+      null,
+      "svg",
+      "qxl.dialog"
+    ],
+    "qxl/dialog/icon/IcoMoonFree/270-cancel-circle.svg": [
+      null,
+      null,
+      "svg",
+      "qxl.dialog"
+    ],
+    "qxl/dialog/icon/IcoMoonFree/272-cross.svg": [
+      null,
+      null,
+      "svg",
+      "qxl.dialog"
+    ],
+    "qxl/dialog/icon/IcoMoonFree/273-checkmark.svg": [
+      null,
+      null,
+      "svg",
+      "qxl.dialog"
+    ]
+  },
+  "translations": {
+    "C": {},
+    "en": {
+      "": "Project-Id-Version: 1.0\nReport-Msgid-Bugs-To: you@your.org\nPOT-Creation-Date: 2011-07-21 20:11+0100\nPO-Revision-Date: 2011-07-21 20:11+0100\nLast-Translator: you <you@your.org>\nLanguage-Team: Team <yourteam@your.org>\nMIME-Version: 1.0\nContent-Type: text/plain; charset=utf-8\nContent-Transfer-Encoding: 8bit\n"
+    }
+  }
+};
